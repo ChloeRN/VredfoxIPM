8ef586787412f2bf458743bebe167774ed230a69
ChloeRNater <chloe.nater@nina.no>
2023-02-20 16:01:21 +0100

    Wrote function for preparing reproduction data

--- RedFox_IPM_Analysis.R
+++ RedFox_IPM_Analysis.R
@@ -7,6 +7,7 @@
 ## Set general parameters
 Amax <- 5 # Number of age classes
 Tmax <- 15  # Number of years
+minYear <- 2004 # First year to consider
 
 ## Load all relevant functions
 
@@ -19,10 +20,33 @@ Tmax <- 15  # Number of years
 #--------------------------------#
 
 ## Set data path/filename
-datafile <- "Data/Cvar.tot_oct-mai_5age.txt"
+wAaH.datafile <- "Data/Cvar.tot_oct-mai_5age.txt"
 
 ## Prepare winter AaH data
-wAaH.data <- wrangleData_winterAaH(datafile = datafile, Amax = Amax)
+wAaH.data <- wrangleData_winterAaH(datafile = wAaH.datafile, 
+                                   Amax = Amax)
 
 
-# 1b)
+# 1b) Reproduction data #
+#-----------------------#
+
+## Set data paths/filenames
+rep.datafiles <- c("Data/P1var_tot.txt", # Placental scar/embryo count
+                   "Data/P2var_tot.txt") # Presence of placental scars/embryos/pregnancy signs
+
+## Prepare reproduction data
+rep.data <- wrangleData_rep(datafiles = rep.datafiles, 
+                            Amax = Amax, 
+                            minYear = minYear)
+
+
+# 1c) Environmental data #
+#------------------------#
+
+
+
+# 1d) Conceptual year information #
+#---------------------------------#
+
+
+
