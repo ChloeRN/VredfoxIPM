5a51d1c31fcb3c3bd503bd066200cb7916e70309
ChloeRNater <chloe.nater@nina.no>
2023-03-01 14:48:41 +0100

    Reset toggles before merging

--- RedFox_IPM_Analysis.R
+++ RedFox_IPM_Analysis.R
@@ -29,7 +29,7 @@ sourceDir('R')
 ## Set "switches" for running different model versions
 
 # Covariate toggles
-fitCov.mH <- TRUE # Fit covariates on mH (harvest effort)
+fitCov.mH <- FALSE # Fit covariates on mH (harvest effort)
 fitCov.Psi <- FALSE # Fit covariates on Psi (rodent abundance)
 rCov.idx <- TRUE # Use discrete vs. continuous rodent covariate
 nLevels.rCov <- 2 # 2-level discrete rodent covariate
