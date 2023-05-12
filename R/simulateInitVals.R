c4c7f62fb740a033efebc844df51975553e5bf1a
ChloeRNater <chloe.nater@nina.no>
2023-05-12 09:36:47 +0200

    Added initial values for missing rodent covariate values

--- R/simulateInitVals.R
+++ R/simulateInitVals.R
@@ -39,6 +39,18 @@ simulateInitVals <- function(nim.data, nim.constants, minN1, maxN1, minImm, maxI
     NHunters[which(is.na(NHunters))] <- mean(NHunters, na.rm = TRUE)
   }
   
+  ## Rodent abundance (continuous)
+  RodentAbundance <- nim.data$RodentAbundance
+  if(NA %in% RodentAbundance){
+    RodentAbundance[which(is.na(RodentAbundance))] <- mean(RodentAbundance, na.rm = TRUE)
+  }
+  
+  ## Rodent abundance (categorical)
+  RodentIndex <- nim.data$RodentIndex
+  if(NA %in% RodentIndex){
+    RodentIndex[which(is.na(RodentIndex))] <- sample(1:nLevels.rCov, length(which(is.na(RodentIndex))))
+  }
+  
   #---------------------------------------------------#
   # Set initial values for vital rate base parameters #
   #---------------------------------------------------#
@@ -286,6 +298,19 @@ simulateInitVals <- function(nim.data, nim.constants, minN1, maxN1, minImm, maxI
     Inits_NHunters <- rep(NA, length(NHunters))
     Inits_NHunters[which(is.na(nim.data$HarvestEffort))] <- NHunters[which(is.na(nim.data$HarvestEffort))]
     InitVals$HarvestEffort <- Inits_NHunters
+  }
+  
+  if(fitCov.Psi){
+    if(rCov.idx & (NA %in% nim.data$RodentIndex)){
+      Inits_RodentIndex <- rep(NA, length(RodentIndex))
+      Inits_RodentIndex[which(is.na(nim.data$RodentIndex))] <- RodentIndex[which(is.na(nim.data$RodentIndex))]
+      InitVals$RodentIndex <- Inits_RodentIndex
+    }
+    if(!rCov.idx & (NA %in% nim.data$RodentAbundance)){
+      Inits_RodentAbundance <- rep(NA, length(RodentAbundance))
+      Inits_RodentAbundance[which(is.na(nim.data$RodentAbundance))] <- RodentAbundance[which(is.na(nim.data$RodentAbundance))]
+      InitVals$RodentAbundance <- Inits_RodentAbundance
+    }
   }
   
   ## Return initial values
