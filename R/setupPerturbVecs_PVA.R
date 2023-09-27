1e8e24e5563c1f05f8c8f93c488c7f65ba6a4f50
ChloeRNater <chloe.nater@nina.no>
2023-09-27 12:39:05 +0200

    Small fix to ensure no perturbation is applied when pert.X = FALSE

--- R/setupPerturbVecs_PVA.R
+++ R/setupPerturbVecs_PVA.R
@@ -79,12 +79,12 @@ setupPerturbVecs_PVA <- function(Tmax, Tmax_sim,
   
   ## Add factors for perturbation period
   if(Tmax_sim > 0){
-    pertFac.mH <- c(pertFac.mH, rep(factor.mH, Tmax_sim))
-    pertFac.mO <- c(pertFac.mO, rep(factor.mO, Tmax_sim))
-    pertFac.S0 <- c(pertFac.S0, rep(factor.S0, Tmax_sim))
-    pertFac.immR <- c(pertFac.immR, rep(factor.immR, Tmax_sim))
-    pertFac.rodent <- c(pertFac.rodent, rep(factor.rodent, Tmax_sim))
-    pertFac.reindeer <- c(pertFac.reindeer, rep(factor.reindeer, Tmax_sim))
+    pertFac.mH <- c(pertFac.mH, rep(ifelse(pert.mH, factor.mH, 1), Tmax_sim))
+    pertFac.mO <- c(pertFac.mO, rep(ifelse(pert.mO, factor.mO, 1), Tmax_sim))
+    pertFac.S0 <- c(pertFac.S0, rep(ifelse(pert.S0, factor.S0, 1), Tmax_sim))
+    pertFac.immR <- c(pertFac.immR, rep(ifelse(pert.immR, factor.immR, 1), Tmax_sim))
+    pertFac.rodent <- c(pertFac.rodent, rep(ifelse(pert.rodent, factor.rodent, 1), Tmax_sim))
+    pertFac.reindeer <- c(pertFac.reindeer, rep(ifelse(pert.reindeer, factor.reindeer, 1), Tmax_sim))
   }
   
   ## List and return perturbation vectors
