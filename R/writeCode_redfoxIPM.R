8f30ab6d33e61b599c8e51043bfe45569b0f79ce
ChloeRNater <chloe.nater@nina.no>
2025-03-06 10:52:27 +0100

    Changed to model log(density) without centering

--- R/writeCode_redfoxIPM.R
+++ R/writeCode_redfoxIPM.R
@@ -253,7 +253,7 @@ writeCode_redfoxIPM <- function(indLikelihood.genData = FALSE){
         
         # Other (natural) mortality hazard rate
         if(fitCov.mO){
-          log(mO[1:Amax, t]) <- log(Mu.mO[1:Amax]) + betaR.mO*RodentAbundance[t+1] + betaD.mO*(log(localN.tot[t])-log(normN)) + epsilon.mO[t]
+          log(mO[1:Amax, t]) <- log(Mu.mO[1:Amax]) + betaR.mO*RodentAbundance[t+1] + betaD.mO*log(localN.tot[t]) + epsilon.mO[t]
         }else{
           log(mO[1:Amax, t]) <- log(Mu.mO[1:Amax]) + epsilon.mO[t]
         }
@@ -430,7 +430,7 @@ writeCode_redfoxIPM <- function(indLikelihood.genData = FALSE){
               }
             }else{
               for(t in 1:(Tmax+1)){
-                log(immR[t]) <- log(Mu.immR) + betaR.immR*RodentAbundance2[t] + betaD.immR*(log(localN.tot[t])-log(normN)) + epsilon.immR[t]
+                log(immR[t]) <- log(Mu.immR) + betaR.immR*RodentAbundance2[t] + betaD.immR*log(localN.tot[t]) + epsilon.immR[t]
                 #log(immR[t]) <- log(Mu.immR) + betaR.immR*RodentAbundance2[t] + epsilon.immR[t]
               }
             }
@@ -765,7 +765,7 @@ writeCode_redfoxIPM <- function(indLikelihood.genData = FALSE){
         
         # Other (natural) mortality hazard rate
         if(fitCov.mO){
-          log(mO[1:Amax, t]) <- log(Mu.mO[1:Amax]) + betaR.mO*RodentAbundance[t+1] + betaD.mO*(log(localN.tot[t])-log(normN)) + epsilon.mO[t]
+          log(mO[1:Amax, t]) <- log(Mu.mO[1:Amax]) + betaR.mO*RodentAbundance[t+1] + betaD.mO*log(localN.tot[t]) + epsilon.mO[t]
         }else{
           log(mO[1:Amax, t]) <- log(Mu.mO[1:Amax]) + epsilon.mO[t]
         }
@@ -931,7 +931,7 @@ writeCode_redfoxIPM <- function(indLikelihood.genData = FALSE){
             #log(immR[1:(Tmax+1)]) <- log(Mu.immR) + betaR.immR*RodentAbundance2[1:(Tmax+1)] + epsilon.immR[1:(Tmax+1)]
             
             for(t in 1:(Tmax+1)){
-              log(immR[t]) <- log(Mu.immR) + betaR.immR*RodentAbundance2[t] + betaD.immR*(log(localN.tot[t])-log(normN)) + epsilon.immR[t]
+              log(immR[t]) <- log(Mu.immR) + betaR.immR*RodentAbundance2[t] + betaD.immR*log(localN.tot[t]) + epsilon.immR[t]
               #log(immR[t]) <- log(Mu.immR) + betaR.immR*RodentAbundance2[t] + epsilon.immR[t]
             }
           }
