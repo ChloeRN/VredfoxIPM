defbcaaf45098d6631e44568496f3d14c986d8ae
ChloeRNater <chloe.nater@nina.no>
2025-03-07 11:08:54 +0100

    Compared model with DD on natural mortality of first age class only
    Results under #81

--- RedFox_IPM_Analysis.R
+++ RedFox_IPM_Analysis.R
@@ -328,7 +328,7 @@ IPM.out <- nimbleMCMC(code = model.setup$modelCode,
 Sys.time() - t1
 
 
-saveRDS(IPM.out, file = "RedFoxIPM_main_singleCensus_DD1_log_alt.rds") 
+saveRDS(IPM.out, file = "RedFoxIPM_main_singleCensus_DD1_log_mO1.rds") 
 #saveRDS(IPM.out, file = "RedFoxIPM_genData1.rds")
 #saveRDS(IPM.out, file = "RedFoxIPM_genData2.rds")
 #saveRDS(IPM.out, file = "RedFoxIPM_survPrior1.rds")
@@ -349,6 +349,17 @@ saveRDS(IPM.out, file = "RedFoxIPM_main_singleCensus_DD1_log_alt.rds")
 ########################
 
 ## Models with density-dependence
+compareModels(Amax = Amax, 
+              Tmax = Tmax, 
+              minYear = minYear, 
+              post.filepaths = c("RedFoxIPM_main_singleCensus_DD1_log_mO1.rds",
+                                 "RedFoxIPM_main_singleCensus_DD1_log.rds",
+                                 "RedFoxIPM_main_singleCensus_combHarvest2.rds"), 
+              model.names = c("Log density effect on mO[1] and immR", 
+                              "Log density effect on mO[a] and immR",
+                              "No density-dependence"), 
+              plotFolder = "Plots/Comp_DensityDep_Age")
+
 compareModels(Amax = Amax, 
               Tmax = Tmax, 
               minYear = minYear, 
