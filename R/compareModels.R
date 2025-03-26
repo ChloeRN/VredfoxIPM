25ca74ac3e0b069189ff453a9e7a08d7b36574b6
ChloeRNater <chloe.nater@nina.no>
2025-03-26 08:24:02 +0100

    Drop last year mO from model comparison plot
    For some reason, this gets estimated with ridiculous uncertainty by some models, making the plot entirely uninformative.

--- R/compareModels.R
+++ R/compareModels.R
@@ -217,6 +217,9 @@ compareModels <- function(Amax, Tmax, minYear, maxYear, logN = FALSE,
   dev.off()
   
   ## Plot posterior summary time series for age-specific parameters
+  sum.data <- sum.data %>%
+    dplyr::filter(!(ParamName == "mO" & Year == max(sum.data$Year)))
+  
   pdf(paste0(plotFolder, "/PosteriorSummaries_TimeSeriesAge.pdf"), width = 8, height = 8)
   for(x in 1:length(plotTS.paramsAge$ParamNames)){
     
