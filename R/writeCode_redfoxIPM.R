886469ed2ac7c5b3ba1688c24e4a2fa0a138f0fc
ChloeRNater <chloe.nater@nina.no>
2025-04-14 11:05:13 +0200

    Fixed small errors in functions

--- R/writeCode_redfoxIPM.R
+++ R/writeCode_redfoxIPM.R
@@ -134,7 +134,7 @@ writeCode_redfoxIPM <- function(indLikelihood.genData = FALSE){
       }
       
       localN.tot[1] <- normN
-      for(t in 2:(Tmax+Tmax_sim)){
+      for(t in 2:Tmax){
         #localN.tot[t] <- survN1[t] + sum(octN[2:Amax, t])
         localN.tot[t] <- sum(N[1:Amax, t])
       }
@@ -837,7 +837,7 @@ writeCode_redfoxIPM <- function(indLikelihood.genData = FALSE){
       }
       
       localN.tot[1] <- normN
-      for(t in 2:(Tmax+Tmax_sim)){
+      for(t in 2:Tmax){
         #localN.tot[t] <- survN1[t] + sum(octN[2:Amax, t])
         localN.tot[t] <- sum(N[1:Amax, t])
       }
